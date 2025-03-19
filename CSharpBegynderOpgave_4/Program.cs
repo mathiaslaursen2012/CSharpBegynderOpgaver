6313996c1a599700b20ee928b35e1fa8aec7b273
Anne-Mari <Anne-Mari@DESKTOP-ANG9J0K>
2025-03-19 18:56:01 +0100

    new update

--- CSharpBegynderOpgave_4/Program.cs
+++ CSharpBegynderOpgave_4/Program.cs
@@ -7,11 +7,7 @@
 
 //While loop der tæller til 10
 int counter = 0;
-while(counter <= 4)
-{
-	Console.WriteLine(counter);
-	counter++;
-}
+
 
 Console.ReadKey();
 //Console.WriteLine("");
@@ -25,14 +21,16 @@ Console.ReadKey();
 
 
 
+
 Random random = new Random();
 int Resultat = random.Next(0, 6);
 Console.WriteLine(Resultat);
-int antalGæt = 3;
+int _ = 3;
 
 while (counter <= 3)
 {
     Console.WriteLine("input a number between 0 & 5");
+    
     int input = Convert.ToInt32(Console.ReadLine());
 
     if (input == Resultat)
